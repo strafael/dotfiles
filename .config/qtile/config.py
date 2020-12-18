6693b7dd3248f728789992787683d24287783139
Rafael Santos <rafaelpsantos@gmail.com>
2020-12-18 19:05:24 -0300

    Update Qtile config

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -14,73 +14,133 @@ mod2 = "control"
 home = os.path.expanduser('~')
 myterm = "alacritty"
 
-keys = [
-    # Essentials
+keys = []
+
+keys.extend([
     Key([mod], "Return", lazy.spawn('alacritty'), desc="Launch terminal"),
     Key([mod], "d", lazy.spawn("rofi -show drun"), desc="Run launcher"),
     Key([mod], "space", lazy.next_layout(), desc="Change to next layout"),
     Key([mod, "shift"], "q", lazy.window.kill(), desc="Kill active window"),
     Key([mod], "0", lazy.spawn('arcolinux-logout'), desc="Open exit menu"),
     Key([mod, "shift"], "r", lazy.restart(), desc="Restart Qtile"),
+])
 
+keys.extend([
     # Switch focus to specific monitor (out of three)
     Key([mod], "w", lazy.to_screen(0), desc='Keyboard focus to monitor 1'),
     Key([mod], "e", lazy.to_screen(1), desc='Keyboard focus to monitor 2'),
     Key([mod], "r", lazy.to_screen(2), desc='Keyboard focus to monitor 3'),
 
-    # Switch focus of monitors
+    # Go to next/previous monitor
     Key([mod], "period", lazy.next_screen(), desc='Move focus to next monitor'),
     Key([mod], "comma", lazy.prev_screen(), desc='Move focus to prev monitor'),
+])
 
-    # Treetab controls
-    # Key([mod], "control", "k", lazy.layout.section_up(), desc="Move up a section in treetab"),
-    # Key([mod], "control", "j", lazy.layout.section_down(), desc="Move down a section in treetab"),
-
-    # Change focus
-    Key([mod], "Up", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "Down", lazy.layout.down(), desc="Move focus down"),
+keys.extend([
     Key([mod], "Left", lazy.layout.left(), desc="Move focus left"),
     Key([mod], "Right", lazy.layout.right(), desc="Move focus right"),
-    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+    Key([mod], "Up", lazy.layout.up(), desc="Move focus up"),
+    Key([mod], "Down", lazy.layout.down(), desc="Move focus down"),
+
     Key([mod], "h", lazy.layout.left(), desc="Move focus left"),
     Key([mod], "l", lazy.layout.right(), desc="Move focus right"),
+    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+])
 
-    # Window controls
+keys.extend([
     Key([mod], "f", lazy.window.toggle_fullscreen(), desc="Toggle fullscreen"),
     Key([mod, "shift"], "space", lazy.window.toggle_floating(), desc="Toggle floating"),
-    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+])
+
+keys.extend([
     Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window left"),
     Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window right"),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
 
-    # Resize window
-    Key([mod, "control"], "h", lazy.layout.grow(), lazy.layout.increase_nmaster(), desc="Expand window / increase number in master pane"),
-    Key([mod, "control"], "l", lazy.layout.shrink(), lazy.layout.decrease_nmaster(), desc="Shrink window / decrease number in master pane"),
-    Key([mod], "n", lazy.layout.normalize(), desc="Normalize window size ratios"),
+    Key([mod, "shift"], "Left", lazy.layout.swap_left(), desc="Move window left"),
+    Key([mod, "shift"], "Right", lazy.layout.swap_right(), desc="Move window right"),
+    Key([mod, "shift"], "Up", lazy.layout.shuffle_up(), desc="Move window up"),
+    Key([mod, "shift"], "Down", lazy.layout.shuffle_down(), desc="Move window down"),
+])
+
+keys.extend([
+    Key([mod, "control"], "Left",
+        # lazy.layout.grow_left(),
+        lazy.layout.shrink(),
+        lazy.layout.decrease_ratio(),
+        lazy.layout.add(),
+    ),
+    Key([mod, "control"], "Right",
+        # lazy.layout.grow_right(),
+        lazy.layout.grow(),
+        lazy.layout.increase_ratio(),
+        lazy.layout.delete(),
+    ),
+    Key([mod, "control"], "Up",
+        # lazy.layout.grow_up(),
+        lazy.layout.grow(),
+        lazy.layout.decrease_nmaster(),
+    ),
+    Key([mod, "control"], "Down",
+        # lazy.layout.grow_down(),
+        lazy.layout.shrink(),
+        lazy.layout.increase_nmaster(),
+    ),
+
+    Key([mod, "control"], "h",
+        # lazy.layout.grow_left(),
+        lazy.layout.shrink(),
+        lazy.layout.decrease_ratio(),
+        lazy.layout.add(),
+    ),
+    Key([mod, "control"], "l",
+        # lazy.layout.grow_right(),
+        lazy.layout.grow(),
+        lazy.layout.increase_ratio(),
+        lazy.layout.delete(),
+    ),
+    Key([mod, "control"], "k",
+        # lazy.layout.grow_up(),
+        lazy.layout.grow(),
+        lazy.layout.decrease_nmaster(),
+    ),
+    Key([mod, "control"], "j",
+        # lazy.layout.grow_down(),
+        lazy.layout.shrink(),
+        lazy.layout.increase_nmaster(),
+    ),
     Key([mod], "m", lazy.layout.maximize(), desc="Toggle window between minimum and maximum sizes"),
+    Key([mod], "n", lazy.layout.reset(), desc="Reset window size ratios"),
+])
 
-    # Stack controls
-    Key([mod, "shift"], "f", lazy.layout.rotate(), lazy.layout.flip(), desc="Switch which side main pane occupies (XmonadTall)"),
+keys.extend([
+    Key([mod, "shift"], "f", lazy.layout.rotate(), lazy.layout.flip(), desc="Switch which side main pane occupies (Monadtall and Tile)"),
     Key([mod], "Tab", lazy.layout.next(), desc="Switch window focus to other pane(s) of stack"),
-    # TODO que isso faz???
     Key([mod, "control"], "Return", lazy.layout.toggle_split(), desc='Toggle between split and unsplit sides of stack'),
+])
 
-    # Dmenu scripts lanched with Ctrl + Alt + Key
+keys.extend([
+    # Key([mod, "control"], "k", lazy.layout.section_up(), desc="Move up a section in treetab"),
+    # Key([mod, "control"], "j", lazy.layout.section_down(), desc="Move down a section in treetab"),
+])
 
-    # Applications launched with Super + Alt + Key
+keys.extend([
     Key([mod, "mod1"], "Left", lazy.spawn('variety -p')),
     Key([mod, "mod1"], "Right", lazy.spawn('variety -n')),
+])
 
-    # Applications launched with Ctrl + Shift + Key
+keys.extend([
     Key([mod2, "shift"], "Escape", lazy.spawn('xfce4-taskmanager')),
+])
 
-    # Screenshots
+keys.extend([
     Key([], "Print", lazy.spawn("flameshot gui"), desc="Select an area and copy to clipboard"),
     Key(["control"], "Print", lazy.spawn("flameshot gui -d 3000"), desc="Wait for 3 seconds, then select an area and copy to clipboard"),
+])
 
-    # MULTIMEDIA KEYS
-
+keys.extend([
     # INCREASE/DECREASE BRIGHTNESS
     Key([], "XF86MonBrightnessUp", lazy.spawn("xbacklight -inc 5")),
     Key([], "XF86MonBrightnessDown", lazy.spawn("xbacklight -dec 5")),
@@ -94,65 +154,7 @@ keys = [
     Key([], "XF86AudioNext", lazy.spawn("playerctl next")),
     Key([], "XF86AudioPrev", lazy.spawn("playerctl previous")),
     Key([], "XF86AudioStop", lazy.spawn("playerctl stop")),
-
-    # RESIZE UP, DOWN, LEFT, RIGHT
-    Key([mod, "control"], "l",
-        lazy.layout.grow_right(),
-        lazy.layout.grow(),
-        lazy.layout.increase_ratio(),
-        lazy.layout.delete(),
-        ),
-    Key([mod, "control"], "Right",
-        lazy.layout.grow_right(),
-        lazy.layout.grow(),
-        lazy.layout.increase_ratio(),
-        lazy.layout.delete(),
-        ),
-    Key([mod, "control"], "h",
-        lazy.layout.grow_left(),
-        lazy.layout.shrink(),
-        lazy.layout.decrease_ratio(),
-        lazy.layout.add(),
-        ),
-    Key([mod, "control"], "Left",
-        lazy.layout.grow_left(),
-        lazy.layout.shrink(),
-        lazy.layout.decrease_ratio(),
-        lazy.layout.add(),
-        ),
-    Key([mod, "control"], "k",
-        lazy.layout.grow_up(),
-        lazy.layout.grow(),
-        lazy.layout.decrease_nmaster(),
-        ),
-    Key([mod, "control"], "Up",
-        lazy.layout.grow_up(),
-        lazy.layout.grow(),
-        lazy.layout.decrease_nmaster(),
-        ),
-    Key([mod, "control"], "j",
-        lazy.layout.grow_down(),
-        lazy.layout.shrink(),
-        lazy.layout.increase_nmaster(),
-        ),
-    Key([mod, "control"], "Down",
-        lazy.layout.grow_down(),
-        lazy.layout.shrink(),
-        lazy.layout.increase_nmaster(),
-        ),
-
-    # FLIP LAYOUT FOR BSP
-    Key([mod, "mod1"], "k", lazy.layout.flip_up()),
-    Key([mod, "mod1"], "j", lazy.layout.flip_down()),
-    Key([mod, "mod1"], "l", lazy.layout.flip_right()),
-    Key([mod, "mod1"], "h", lazy.layout.flip_left()),
-
-    # MOVE WINDOWS UP OR DOWN MONADTALL/MONADWIDE LAYOUT
-    Key([mod, "shift"], "Up", lazy.layout.shuffle_up()),
-    Key([mod, "shift"], "Down", lazy.layout.shuffle_down()),
-    Key([mod, "shift"], "Left", lazy.layout.swap_left()),
-    Key([mod, "shift"], "Right", lazy.layout.swap_right()),
-]
+])
 
 group_names = [
     ("1", {"layout": "max"}),
@@ -191,30 +193,10 @@ layout_theme = {
 
 layouts = [
     layout.MonadTall(**layout_theme),
-    layout.Bsp(**layout_theme),
-    layout.Columns(**layout_theme),
     layout.Max(**layout_theme),
-    layout.MonadWide(**layout_theme),
     layout.Tile(shift_windows=True, **layout_theme),
-    layout.VerticalTile(**layout_theme),
-    layout.RatioTile(**layout_theme),
     layout.Matrix(**layout_theme),
-    layout.Zoomy(**layout_theme),
-    layout.Stack(num_stacks=2),
-    layout.TreeTab(
-        font = "Ubuntu",
-        fontsize = 10,
-        sections = ["FIRST", "SECOND"],
-        section_fontsize = 11,
-        bg_color = "141414",
-        active_bg = "90C435",
-        active_fg = "000000",
-        inactive_bg = "384323",
-        inactive_fg = "a0a0a0",
-        padding_y = 5,
-        section_top = 10,
-        panel_width = 320,
-    ),
+    layout.Stack(num_stacks=2, **layout_theme),
     layout.Floating(**layout_theme),
 ]
 
@@ -255,7 +237,7 @@ widgets_list = [
 
     # Current layout
     widget.CurrentLayoutIcon(
-        custom_icon_paths = [os.path.expanduser("~/.config/qtile/icons")],
+        # custom_icon_paths = [os.path.expanduser("~/.config/qtile/icons")],
         padding = 0,
         scale = 0.6
     ),
