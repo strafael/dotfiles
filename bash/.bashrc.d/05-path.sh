ba577548964f2efbb47b1f99ad9d2584625ef16b
Rafael Pereira <rafaelpsantos@gmail.com>
2025-09-04 12:48:08 +0100

    add shellopts

--- bash/.bashrc.d/05-path.sh
+++ bash/.bashrc.d/05-path.sh
@@ -3,6 +3,5 @@ append_path()  { case ":$PATH:" in *":$1:"*) ;; *) PATH="$PATH:$1";; esac; }
 
 prepend_path "$HOME/bin"
 prepend_path "$HOME/.local/bin"
-prepend_path "${ASDF_DATA_DIR:-$HOME/.asdf}/shims"
 
 export PATH
---
+++ bash/.bashrc.d/50-asdf.sh
@@ -0,0 +1,1 @@
+prepend_path "${ASDF_DATA_DIR:-$HOME/.asdf}/shims"
