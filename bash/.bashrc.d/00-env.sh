fadb8441e18ef4f13af2e0b89c00979df311c179
Rafael Pereira <rafaelpsantos@gmail.com>
2025-09-04 11:28:08 +0100

    base fonts, tmux, alacritty

--- bash/.bashrc.d/00-env.sh
+++ bash/.bashrc.d/00-env.sh
@@ -1,2 +1,6 @@
-export EDITOR=/usr/bin/nvim
+: "${XDG_CONFIG_HOME:=$HOME/.config}"
+: "${XDG_DATA_HOME:=$HOME/.local/share}"
+: "${XDG_CACHE_HOME:=$HOME/.cache}"
 
+export EDITOR="/usr/bin/nvim"
+export VISUAL="$EDITOR"
