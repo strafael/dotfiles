6078ee2af27c3eab0bbbd824941faf69f4487fea
Rafael Pereira <rafaelpsantos@gmail.com>
2025-09-05 10:39:35 +0100

    add eza

--- bash/.bashrc.d/20-aliases.sh
+++ bash/.bashrc.d/20-aliases.sh
@@ -3,3 +3,15 @@ alias cp='cp -i'
 alias mv='mv -i'
 alias rm='rm -i'
 
+alias l="eza --icons=always"
+alias ls="eza --icons=always"
+alias ll="eza -lg --icons=always"
+alias la="eza -lag --icons=always"
+alias lt="eza -lTg --icons=always"
+alias lt2="eza -lTg --level=2 --icons=always"
+alias lt3="eza -lTg --level=3 --icons=always"
+alias lt4="eza -lTg --level=4 --icons=always"
+alias lta="eza -lTag --icons=always"
+alias lta2="eza -lTag --level=2 --icons=always"
+alias lta3="eza -lTag --level=3 --icons=always"
+alias lta4="eza -lTag --level=4 --icons=always"
